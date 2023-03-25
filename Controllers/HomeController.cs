26c0b6410b903ab50aab94427025ea7673cf3179
Nicolas <n.serna@utp.edu.co>
2023-03-25 16:10:19 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace Punto1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        public static List<Usuario> usuarios = new List<Usuario>()
+        {
+            new Usuario
+            {
+                nombre = "Nicolas",
+                cc = 1112039498,
+                direccion = "cra 7, casa 4",
+                telefono = "3218810027",
+                nacimiento = DateTime.Parse("2004-07-29")
+            },
+            new Usuario
+            {
+                nombre = "Nicolas",
+                cc = 1112039498,
+                direccion = "cra 7, casa 4",
+                telefono = "3218810027",
+                nacimiento = DateTime.Parse("2004-07-29")
+            }
+        };
+        [HttpGet("Consultar")]
+        public List<Usuario> Consultar()
+        {
+            return usuarios;
+        }
+        [HttpGet("Consultar 2")]
+        public string Consultar2()
+        {
+            return "Hola Mundo 2";
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace Punto1
+{
+    public class Usuario
+    {   
+        public string nombre { get; set; }
+
+        public int cc { get; set; }
+
+        public string direccion { get; set;}
+
+        public string telefono { get; set;}
+
+        public DateTime nacimiento { get; set; }
+    }
+}
